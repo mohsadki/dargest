4a91302003ef15246710e64cb3158d3622c1f714
simohamed <simohamed.seddik@outlook.fr>
2016-02-13 21:27:47 +0000

    te

---
+++ dargest/src/Controller/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class UsersController extends AppController
+{
+
+	public function index()
+	{
+	
+	}
+	public function login()
+	{
+	
+	}
+}
+
+?>
